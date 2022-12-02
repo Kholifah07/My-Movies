dfe7d472eac46ef618ba3857c887684c119f5aec
Kholifah07 <siti.nurkholifah@students.utdi.ac.id>
2022-12-02 14:45:53 +0700

    My Movie

---
+++ app/src/main/java/com/example/mymovie/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.mymovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.mymovie.databinding.ActivityMainBinding
+import com.example.mymovie.MovieAdapter
+import com.example.mymovie.MovieViewModel
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel: MovieViewModel
+    private lateinit var movieAdapter : MovieAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        prepareRecyclerView()
+        viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
+        viewModel.getPopularMovies()
+        viewModel.observeMovieLiveData().observe(this, Observer { movieList -> movieAdapter.setMovieList(movieList)
+        })
+    }
+
+    private fun prepareRecyclerView() {
+        movieAdapter = MovieAdapter()
+        binding.rvMovies.apply {
+            layoutManager = GridLayoutManager(applicationContext,2)
+            adapter = movieAdapter
+        }
+    }
+}
